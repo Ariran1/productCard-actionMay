d88b114dd98160f1b2f6a1b85b453813d447b575
ariran5 <aria-n@mail.ru>
2017-05-02 17:02:58 +0300

    Улучшен скрипт
    Убирается обработчик на скролл после скролла

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 -function() {
 	document.addEventListener('DOMContentLoaded',function(){
-		this.addEventListener('scroll',function(){
-			this.querySelector('[data-style="productCard__buyBlock"]').classList.add('orange');
-		});
+		this.addEventListener('scroll',setOrange);
 	});
+	function setOrange(){
+		this.querySelector('[data-style="productCard__buyBlock"]').classList.add('orange');
+		this.removeEventListener('scroll',setOrange);	
+	}
 }();
