bbb49f3527665f67ff4c0d33e8ea16eece699619
ariran5 <aria-n@mail.ru>
2017-05-02 16:59:01 +0300

    Добавлен скрипт и добавлены стили анимации

--- main.js
+++ main.js
@@ -1,38 +1,7 @@
-const elements = ['.n-products-page__gender','.n-products-page__categories'];
-
-class isVisible {
-	constructor() {
-		for (var i = 0; i < elements.length; i++) {
-
-			this.isDisplay(elements[i]);
-
-		}
-	}
-
-	isDisplay(element) {
-				console.log(element,'1');
-		
-		let b = document.querySelectorAll(element);
-
-		for (var i = 0; i < b.length; i++) {
-
-			let a = b[i];
-
-			while(true) {
-				if (!a.parentNode) return;
-				if (getComputedStyle(a).display === 'none') {
-					this.isDisplayed = true;
-						console.log(element,'Этот элемент скрыт с помощью элемента ', a);
-					return;
-				}
-				a = a.parentNode;
-			}
-		}
-	}
-
-	isOpacity() {
-		getComputedStyle(elements[i]).opacity <= 0.2 
-	}
-}
-
-new isVisible();
+-function() {
+	document.addEventListener('DOMContentLoaded',function(){
+		this.addEventListener('scroll',function(){
+			this.querySelector('[data-style="productCard__buyBlock"]').classList.add('orange');
+		});
+	});
+}();
